460bb8a5ce47fd4ff51643f077df1a13b04ff457
Chukwurado <chukwurado.umeaka98@gmail.com>
2019-12-22 18:58:27 -0500

    Initial Commit

---
+++ flix/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flix
+//
+//  Created by Chukwurado Umeaka on 12/22/19.
+//  Copyright © 2019 Chukwurado Umeaka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
