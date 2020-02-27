408b6e33816cdb828c47ad1cc8f1b2e591c78e37
Chukwurado <chukwurado.umeaka98@gmail.com>
2020-02-27 13:25:54 -0500

    Part 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Chukwurado Umeaka on 2/2/20.
+//  Copyright © 2020 Chukwurado Umeaka. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
